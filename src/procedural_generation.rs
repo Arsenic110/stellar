f92b85dbd22eb5addfd2980b4b7236fcc7eed6f8
Arsenic110 <kubarodak2003@outlook.com>
2026-01-15 19:07:29 +0000

    migrate to new module definition structure

--- src/procedural_generation.rs
+++ src/procedural_generation.rs
@@ -1,4 +1,4 @@
 pub mod gen_icon;
 pub mod gen_planet;
 pub mod gen_star;
-pub mod gen_system;
+//pub mod gen_system;
---
+++ src/stellar_core/solar_system.rs
@@ -0,0 +1,72 @@
+pub mod orbit;
+pub use orbit::Orbit;
+
+pub mod planet;
+pub use planet::Planet;
+
+pub mod star;
+pub use star::Star;
+
+pub mod celestial_body;
+//pub use celestial_body::CelestialBody;
+
+pub mod barycenter;
+pub use barycenter::Barycenter;
+
+use bevy::prelude::*;
+
+pub struct SolarSystemPlugin;
+impl Plugin for SolarSystemPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(Startup, setup_solar_system)
+            .add_systems(Update, update_solar_system);
+    }
+}
+
+
+fn setup_solar_system(
+    mut commands: Commands, 
+    mut images: ResMut<Assets<Image>>,
+) {
+
+
+
+    // let system = crate::procedural_generation::gen_system::gen_system("eriku");
+
+    // let mut stars = vec![];
+    // let mut planets = vec![];
+
+    // for x in &system {
+    //     match x {
+    //         CelestialBody::Barycenter(_) => {},
+    //         CelestialBody::Star(s) => stars.push(s),
+    //         CelestialBody::Planet(p) => planets.push(p),
+    //     }
+    // }
+
+    // for (i, star) in stars.into_iter().enumerate() {
+    //     commands.spawn(Star::get_bundle(
+    //         star.clone(), 
+    //         i as f32 * 1000.0, 
+    //         10.0, 
+    //         &mut images
+    //     ));
+    // }
+
+    // for (i, planet) in planets.into_iter().enumerate() {
+    //     commands.spawn(Planet::get_bundle(
+    //         planet.clone(), 
+    //         i as f32 * 1000.0, 
+    //         1000.0, 
+    //         &mut images
+    //     ));
+    // }
+
+
+}
+
+fn update_solar_system(_bodies: Query<&mut Planet>, mut _gizmos: Gizmos) {
+
+}
+
